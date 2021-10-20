0e23f049215c4e10dff7f7911cc1e8e853dce33c
aguspecuch <agustina.pecuch@hotmail.com>
2021-10-20 09:39:59 -0300

    first commit

---
+++ src/main/java/ar/com/alkemy/warmupchallenge/WarmUpChallengeApplication.java
@@ -0,0 +1,13 @@
+package ar.com.alkemy.warmupchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmUpChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmUpChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/alkemy/warmupchallenge/WarmUpChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.alkemy.warmupchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmUpChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
